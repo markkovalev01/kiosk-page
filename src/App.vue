cb08df140f3ed0ea1478ad922a39dfc1b334af5c
Mark <kovalevm9609@yandex.ru>
2021-05-08 17:18:45 +0900

    created page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  v-app
+    v-main
+      a-button-panel
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AButtonPanel from '@/components/AButtonPanel'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    AButtonPanel
+  },
+
+  data: () => ({
+    //
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AButtonPanel/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  div
+    v-container(class="pa-16"
+      align="center"
+      justify="center"
+    )
+      v-flex(class="pa-16"
+        align="center"
+        justify="center"
+      )
+        v-row
+          v-col(align="center"
+            justify="center")
+            v-btn(elevation="10" dark color="#8b2639" x-large class="justify-center" href="https://bgpu.ru/sveden/education/eduAccred/").text-h4 Образовательные программы
+        v-row
+          v-col(align="center"
+            justify="center")
+            v-btn(elevation="10" dark color="#8b2639" x-large class="justify-center" href="https://bgpu.ru/sveden/document/").text-h4 Нормативные документы
+        v-row
+          v-col(align="center"
+            justify="center")
+            v-btn(elevation="10" dark color="#8b2639" x-large class="justify-center" href="https://bgpu.ru").text-h4 Сайт университета
+</template>
+
+<script>
+export default {
+  name: 'AButtonPanel'
+}
+</script>
+
+<style scoped>
+
+</style>
